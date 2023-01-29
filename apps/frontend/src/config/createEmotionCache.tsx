cd4ad588b3f195f68afca274160418777959f216
Polapob <71330294+Polapob@users.noreply.github.com>
2023-01-29 18:37:24 +0700

    feat: initialize next.js with mui

---
+++ apps/frontend/src/config/createEmotionCache.tsx
@@ -0,0 +1,4 @@
+import createCache from "@emotion/cache";
+export default function createEmotionCache() {
+  return createCache({ key: "css", prepend: true });
+}
--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import "@/styles/globals.css";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
 import type { AppProps } from "next/app";
+import createEmotionCache from "@/config/createEmotionCache";
+import theme from "@/config/theme";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }

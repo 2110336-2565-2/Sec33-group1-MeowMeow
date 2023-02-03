a908f0411497be354e3b2592dd4bf88a4315ecd5
Polapob <71330294+Polapob@users.noreply.github.com>
2023-02-04 01:16:37 +0700

    feat(login-page): implement login static page

---
+++ apps/frontend/src/components/LoginPage/LoginPageContainer.tsx
@@ -0,0 +1,53 @@
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import { ReactNode } from "react";
+const ResponsiveBox = styled(Box)(({ theme }) => {
+  return {
+    backgroundColor: "#FFFFFF",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      padding: "24px",
+      margin: "12px",
+      borderRadius: "12px",
+    },
+
+    [theme.breakpoints.up("sm")]: {
+      width: "500px",
+      padding: "32px",
+      borderRadius: "16px",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "720px",
+      padding: "50px",
+      borderRadius: "20px",
+    },
+  };
+});
+
+interface ILoginPageContainerProps {
+  children?: ReactNode;
+}
+
+const LoginPageContainer = ({ children }: ILoginPageContainerProps) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+        background:
+          "linear-gradient(90deg, #FF9A9E 0%, #FAD0C4 99%, #FAD0C4 100%)",
+      }}
+    >
+      <ResponsiveBox>{children}</ResponsiveBox>
+    </Box>
+  );
+};
+
+export default LoginPageContainer;

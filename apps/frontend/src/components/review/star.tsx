d618abb1040d1a869b4dfea01e2e3e5b7d0c3479
TikhampornSky <tikhamporntan@gmail.com>
2023-02-03 23:11:44 +0700

    feat: add star component

---
+++ apps/frontend/src/components/review/star.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import Rating from "@mui/material/Rating";
+import Box from "@mui/material/Box";
+import StarIcon from "@mui/icons-material/Star";
+import { Grid, Stack } from "@mui/material";
+
+const labels: { [index: string]: string } = {
+  0: "Not Recommended",
+  0.5: "Very Bad",
+  1: "Bad",
+  1.5: "Very Not Okay ",
+  2: "Not Okay",
+  2.5: "Medium",
+  3: "Okay",
+  3.5: "Good",
+  4: "Good Guide!",
+  4.5: "Excellent",
+  5: "Excellent! Guide",
+};
+
+function getLabelText(value: number) {
+  return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`;
+}
+
+export default function Star() {
+  const [value, setValue] = React.useState<number | null>(2);
+  const [hover, setHover] = React.useState(-1);
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        width: 200,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Grid item>
+        <Rating
+          name="hover-feedback"
+          value={value}
+          precision={0.5}
+          getLabelText={getLabelText}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          onChangeActive={(event, newHover) => {
+            setHover(newHover);
+          }}
+        />
+      </Grid>
+      <Grid item>
+        {value !== null && (
+          <Box sx={{ marginTop: 2 }}>
+            {labels[hover !== -1 ? hover : value]}
+          </Box>
+        )}
+      </Grid>
+    </Grid>
+  );
+}
---
+++ apps/frontend/src/pages/review/main.tsx
@@ -0,0 +1,56 @@
+import Star from "@/components/review/star";
+import theme from "@/config/theme";
+import { Box, Grid, Paper, styled } from "@mui/material";
+import React from "react";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+export default function Review() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: theme.palette.grey["100"],
+        margin: 5,
+        padding: 5,
+        borderRadius: 5,
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid
+          container
+          xs={12}
+          justifyContent="center"
+          alignItems="center"
+          fontWeight={600}
+          fontSize={30}
+        >
+          Review
+        </Grid>
+        <Grid item xs={12}>
+          Natee Niparnan
+        </Grid>
+        <Grid container xs={12} justifyContent="center" alignItems="center">
+          <Star />
+        </Grid>
+        <Grid item xs={12}>
+          <Item> 4 </Item>
+        </Grid>
+        <Grid item xs={12}>
+          <Item> 5 </Item>
+        </Grid>
+        <Grid item xs={12}>
+          <Item> 6 </Item>
+        </Grid>
+        <Grid item xs={12}>
+          <Item> 7 </Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}

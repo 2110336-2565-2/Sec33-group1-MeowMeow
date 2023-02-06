812b914c142a149421d0e9d6ac4d0a7c8d4ead7a
Kenshiro <mahasamutngeon@gmail.com>
2023-02-07 03:45:01 +0700

    feat: add basic users module and controller

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule { }

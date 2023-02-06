5941a40cb580c48c29ddb9893838377793dff26a
Kenshiro <mahasamutngeon@gmail.com>
2023-02-07 03:45:01 +0700

    feat: add basic users module and controller

---
+++ apps/backend/src/users/controllers/users/users.controller.ts
@@ -0,0 +1,8 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/createUser.dto';
+
+@Controller('users')
+export class UsersController {
+  @Post('register')
+  createUser(@Body() data: CreateUserDto) {}
+}

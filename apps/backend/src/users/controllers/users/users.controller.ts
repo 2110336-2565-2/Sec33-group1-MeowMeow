93a9097dc9d32b859a4d50d64b8e32004434eb10
Kenshiro <mahasamutngeon@gmail.com>
2023-02-08 18:31:10 +0700

    chore(users): edit dummy message

--- apps/backend/src/users/controllers/users/users.controller.ts
+++ apps/backend/src/users/controllers/users/users.controller.ts
@@ -4,5 +4,7 @@ import { CreateUserDto } from 'src/users/dto/createUser.dto';
 @Controller('users')
 export class UsersController {
   @Post('register')
-  createUser(@Body() data: CreateUserDto) {}
+  createUser(@Body() data: CreateUserDto) {
+    return { msg: 'create user successfully' };
+  }
 }

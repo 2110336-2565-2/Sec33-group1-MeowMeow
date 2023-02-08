7eae8ee2eda4ce8ee6368280551ea520e8fab3a5
K. Jetnipit <jetnipit54@gmail.com>
2023-02-08 18:50:07 +0700

    feat(error-handling): add various error type that can be thrown from auth service

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, HttpStatus, Post, Body, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginRequest, LoginResponse } from './auth.dto';
-import { InvalidRequestError } from './auth.commons';
+import {
+  InvalidAuthenticationError,
+  InvalidRequestError,
+} from './auth.commons';
 
 @Controller()
 export class AppController {
@@ -10,14 +13,19 @@ export class AppController {
   @Post('/sign-in')
   signIn(@Body() req: LoginRequest, @Res({ passthrough: true }) res) {
     let resBody: LoginResponse;
-    let statusCode = HttpStatus.OK;
+    let statusCode = HttpStatus.CREATED;
     try {
       resBody = this.authService.login(req);
     } catch (e) {
       if (e instanceof InvalidRequestError) {
         statusCode = HttpStatus.BAD_REQUEST;
+        resBody = { message: 'invalid request' };
+      } else if (e instanceof InvalidAuthenticationError) {
+        statusCode = HttpStatus.UNAUTHORIZED;
+        resBody = { message: 'incorrect email or password' };
       } else {
         statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+        resBody = { message: 'internal server error' };
       }
     }
     return res.status(statusCode).send(resBody);

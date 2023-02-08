e0d400a4946e092535377f40cc198033552ba546
MinitrueSecretary <wipugeng@hotmail.com>
2023-02-08 22:11:25 +0700

    feat: implemented validation process for reviews

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { ServiceService } from './review/service/service.service';
+import { ReviewService } from './review/service/review/review.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, ServiceService, ReviewService],
 })
 export class AppModule {}

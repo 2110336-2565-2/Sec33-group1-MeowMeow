f000ae26ac7a03a7852c19cdf7e533c324f767a7
K. Jetnipit <jetnipit54@gmail.com>
2023-02-08 19:00:19 +0700

    refactor(error-message): return message embedded in original error instead

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -19,10 +19,10 @@ export class AppController {
     } catch (e) {
       if (e instanceof InvalidRequestError) {
         statusCode = HttpStatus.BAD_REQUEST;
-        resBody = { message: 'invalid request' };
+        resBody = { message: e.message };
       } else if (e instanceof InvalidAuthenticationError) {
         statusCode = HttpStatus.UNAUTHORIZED;
-        resBody = { message: 'incorrect email or password' };
+        resBody = { message: e.message };
       } else {
         statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
         resBody = { message: 'internal server error' };

c7de84a3e106b5c561857c451bd603d2e308569c
MinitrueSecretary <wipugeng@hotmail.com>
2023-02-08 22:43:22 +0700

    feat: add a demo service for reviews

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
-import { ServiceService } from './review/service/service.service';
 import { ReviewService } from './review/service/review/review.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService, ServiceService, ReviewService],
+  providers: [AppService, PrismaService, ReviewService],
 })
 export class AppModule {}

e743964f935d00a7545cb16961e0f329f2253f74
JinDamanee <88868930+JinDamanee2544@users.noreply.github.com>
2023-02-09 10:35:49 +0700

    fix: fix header and footer not span fully the viewport

--- apps/frontend/src/components/LandingPage/navbar.tsx
+++ apps/frontend/src/components/LandingPage/navbar.tsx
@@ -7,22 +7,24 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
 import theme from "@/config/theme";
+import { Container } from "@mui/material";
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1, paddingBottom: 3 }}>
       <AppBar position="static" elevation={4} style={{ background: "#FFFF" }}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2, color: theme.palette.primary["main"] }}
-          >
-            <CatchingPokemonIcon />
-          </IconButton>
-          {/* <Box
+        <Container maxWidth="xl">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2, color: theme.palette.primary["main"] }}
+            >
+              <CatchingPokemonIcon />
+            </IconButton>
+            {/* <Box
                 component="img"
                 sx={{
                 height: 30,
@@ -32,18 +34,19 @@ export default function Navbar() {
                 }}
                 src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
             /> */}
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, fontWeight: "bold" }}
-          >
-            GuideKai
-          </Typography>
-          <Button variant="outlined" style={{ margin: "10px" }}>
-            LOGIN
-          </Button>
-          <Button variant="contained">SIGN UP</Button>
-        </Toolbar>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, fontWeight: "bold" }}
+            >
+              GuideKai
+            </Typography>
+            <Button variant="outlined" style={{ margin: "10px" }}>
+              LOGIN
+            </Button>
+            <Button variant="contained">SIGN UP</Button>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );

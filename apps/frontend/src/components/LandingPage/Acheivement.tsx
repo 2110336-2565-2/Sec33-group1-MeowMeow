b3957e984f8929252c1cbc7a420b5c9ff115a508
JinDamanee <88868930+JinDamanee2544@users.noreply.github.com>
2023-02-09 11:02:16 +0700

    refactor: change files name that correspond with views

---
+++ apps/frontend/src/components/LandingPage/Acheivement.tsx
@@ -0,0 +1,122 @@
+import { Button, Grid, Container, Paper, styled } from "@mui/material";
+import React from "react";
+import theme from "@/config/theme";
+import PromotionBox from "./promotionBox";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import GroupsIcon from "@mui/icons-material/Groups";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+}));
+
+export default function Promotion() {
+    return (
+        <Grid
+            maxWidth="xl"
+            container
+            justifyContent="center"
+            alignItems="center"
+            columnGap={5}
+            style={{ padding: "10px" }}
+            sx={{ flexDirection: { xs: "column", sm: "row" } }}
+        >
+            <Grid item sm={3} xs={12} sx={{ height: "auto" }}>
+                <Grid container justifyContent="center" alignItems="center">
+                    <PromotionBox>
+                        <Grid
+                            container
+                            style={{
+                                fontSize: "25px",
+                                fontWeight: "bold",
+                                paddingBottom: "25px",
+                                height: "auto",
+                            }}
+                        >
+                            <StarBorderIcon style={{ color: theme.palette.primary.main }} />{" "}
+                            4.8 Rating
+                        </Grid>
+                        <Grid container style={{ paddingBottom: "15px" }}>
+                            <GroupsIcon />{" "}
+                            <span style={{ color: theme.palette.primary.main }}>
+                                {" "}
+                                +800k &nbsp;{" "}
+                            </span>{" "}
+                            Members
+                        </Grid>
+                        <Grid container style={{ paddingBottom: "30px" }}>
+                            More than 2 billion people in thailand using Guidekai to find the
+                            matched guide
+                        </Grid>
+                        <Grid container style={{ color: theme.palette.primary.main }}>
+                            <Button variant="text">
+                                Join Our Community <ArrowForwardIcon />{" "}
+                            </Button>
+                        </Grid>
+                    </PromotionBox>
+                </Grid>
+            </Grid>
+            <Grid item sm={3} xs={12} sx={{ height: "auto" }}>
+                <Grid container justifyContent="center" alignItems="center">
+                    <PromotionBox>
+                        <Grid
+                            container
+                            style={{
+                                fontSize: "25px",
+                                fontWeight: "bold",
+                                paddingBottom: "25px",
+                            }}
+                        >
+                            <EmojiEventsIcon style={{ color: theme.palette.primary.main }} />{" "}
+                            Awards
+                        </Grid>
+                        <Grid container style={{ paddingBottom: "15px" }}>
+                            <GitHubIcon style={{ color: theme.palette.primary.main }} /> Best
+                            2021 Github
+                        </Grid>
+                        <Grid container style={{ paddingBottom: "35px" }}>
+                            Guidekai is voted as the popular project on Github in 2021 and won
+                            the best new project 2021 award
+                        </Grid>
+                        <Grid container style={{}}>
+                            <Button variant="text">
+                                Go To Awards <ArrowForwardIcon />{" "}
+                            </Button>
+                        </Grid>
+                    </PromotionBox>
+                </Grid>
+            </Grid>
+            <Grid item sm={4} xs={12}>
+                <Grid
+                    container
+                    style={{ paddingBottom: "5px", color: theme.palette.primary.main }}
+                >
+                    Our Achievement
+                </Grid>
+                <Grid
+                    container
+                    style={{
+                        paddingBottom: "15px",
+                        fontWeight: "bold",
+                        fontSize: "30px",
+                    }}
+                >
+                    We connect You to The Adventure Life
+                </Grid>
+                <Grid container style={{ paddingBottom: "35px" }}>
+                    Guidekai aims to be the friend of your trip. We take care of your
+                    journey at every step to ensure you feel comfortable.
+                </Grid>
+                <Grid container style={{}}>
+                    <Button variant="contained"> Discover more </Button>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+}
---
+++ apps/frontend/src/components/LandingPage/AcheivementBox.tsx
@@ -0,0 +1,23 @@
+import theme from "@/config/theme";
+import { Padding } from "@mui/icons-material";
+import { Box, Grid } from "@mui/material";
+import React from "react";
+
+export default function PromotionBox({ children }: any) {
+    return (
+        <Grid
+            container
+            sx={{
+                width: 300,
+                height: { xs: 300, sm: 350 },
+                backgroundColor: "#ffffff",
+                boxShadow: 5,
+                borderRadius: "16px",
+                padding: "10px",
+                marginBottom: { xs: "25px", sm: "0px" },
+            }}
+        >
+            {children}
+        </Grid>
+    );
+}

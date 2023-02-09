a936d8da73d362f044aed7cce375732e9ea560e9
Kenshiro <mahasamutngeon@gmail.com>
2023-02-10 00:33:41 +0700

    chore(users): import prisma module

--- apps/backend/src/users/users.module.ts
+++ apps/backend/src/users/users.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersController } from './controllers/users/users.controller';
 
 @Module({
   controllers: [UsersController],
+  imports: [PrismaModule],
 })
 export class UsersModule {}

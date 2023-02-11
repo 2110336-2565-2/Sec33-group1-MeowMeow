9a5bddbd6b6d04979e8f0a54fe68b57b968fa9dd
MinitrueSecretary <wipugeng@hotmail.com>
2023-02-11 19:33:16 +0700

    feat: add review service (work on reviewerID pending)

--- apps/backend/src/review/service/review/review.service.ts
+++ apps/backend/src/review/service/review/review.service.ts
@@ -1,14 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, HttpStatus } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { createReviewType } from 'src/review/utils/types';
 
 @Injectable()
 export class ReviewService {
+  constructor(private prisma: PrismaService) {}
   private demo_review = [];
-  createReview(reviewData: createReviewType) {
-    this.demo_review.push(reviewData);
+  async createReview(reviewData: createReviewType) {
+    const rating: number = reviewData.rating;
+    if (rating < 0 || rating > 5 || rating % 0.5 != 0) {
+      throw new HttpException(
+        'Invalid rating, it should be between 0 and 5 and multiple of 0.5',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
 
-    console.log('Pushed Data');
-    console.log(reviewData);
+    const comment: string = reviewData.comment;
+    if (comment.length > 200) {
+      throw new HttpException(
+        'Comment too long, it should not exceed 200 characters.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    // this.demo_review.push(reviewData);
+    const review = await this.prisma.review.create({
+      data: {
+        publishDate: new Date(),
+        score: reviewData.rating,
+        text: reviewData.comment,
+        reviewerId: 20, //holder
+        guideId: parseInt(reviewData.guideID),
+      },
+    });
+    // console.log('Pushed Data');
+    console.log(review);
+
     return {};
   }
 }

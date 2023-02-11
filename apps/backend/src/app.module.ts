1846b8ba6fe30674d0d11198ef274596e081f406
MinitrueSecretary <wipugeng@hotmail.com>
2023-02-11 19:40:25 +0700

    fix: add missing import in app that made review API failed

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -9,10 +9,11 @@ import { UsersModule } from './users/users.module';
 import { ReviewService } from './review/service/review/review.service';
 import { GuidesController } from './guides/guides.controller';
 import { GuidesService } from './guides/service/guides/guides.service';
+import { ReviewController } from './review/review.controller';
 
 @Module({
   imports: [AuthModule, UsersModule],
-  controllers: [AppController, GuidesController],
+  controllers: [AppController, GuidesController,ReviewController],
   providers: [AppService, PrismaService, ReviewService, GuidesService],
 })
 export class AppModule {}

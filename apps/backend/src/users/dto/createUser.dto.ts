d8dd71543f71dfc365951452b4fec939f334076a
Kenshiro <mahasamutngeon@gmail.com>
2023-02-11 11:49:10 +0700

    feat(users): add validation rules

--- apps/backend/src/users/dto/createUser.dto.ts
+++ apps/backend/src/users/dto/createUser.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 import { Role, Guide, Review } from 'database';
 
 export class CreateUserDto {
@@ -9,18 +10,24 @@ export class CreateUserDto {
   // createdAt: Date
 
   @ApiProperty({ type: String })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @ApiProperty({ type: String })
+  @IsNotEmpty()
   username: string;
 
   @ApiProperty({ type: String })
+  @IsNotEmpty()
   hashPassword: string;
 
   @ApiProperty({ type: String })
+  @IsNotEmpty()
   firstName: string;
 
   @ApiProperty({ type: String })
+  @IsNotEmpty()
   lastName: string;
 
   // @ApiProperty({ enum: Role, enumName: 'Role' })
@@ -29,6 +36,6 @@ export class CreateUserDto {
   // @ApiPropertyOptional()
   // Guide?: Guide
 
-  @ApiProperty({ isArray: true })
-  Review: Review[];
+  // @ApiProperty({ isArray: true })
+  // Review: Review[];
 }

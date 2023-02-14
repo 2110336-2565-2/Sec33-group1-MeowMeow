ee933703b98cd529451f468b6ba18ddfd4929f05
K. Jetnipit <jetnipit54@gmail.com>
2023-02-14 16:04:25 +0700

    refactor: remove comments

--- apps/backend/src/users/dto/createUser.dto.ts
+++ apps/backend/src/users/dto/createUser.dto.ts
@@ -1,14 +1,7 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
-import { Role, Guide, Review } from 'database';
 
 export class CreateUserDto {
-  // @ApiProperty({ type: Number })
-  // id: number
-
-  // @ApiProperty({ type: Date })
-  // createdAt: Date
-
   @ApiProperty({ type: String })
   @IsEmail()
   @IsNotEmpty()
@@ -29,13 +22,4 @@ export class CreateUserDto {
   @ApiProperty({ type: String })
   @IsNotEmpty()
   lastName: string;
-
-  // @ApiProperty({ enum: Role, enumName: 'Role' })
-  // role: Role = Role.USER
-
-  // @ApiPropertyOptional()
-  // Guide?: Guide
-
-  // @ApiProperty({ isArray: true })
-  // Review: Review[];
 }

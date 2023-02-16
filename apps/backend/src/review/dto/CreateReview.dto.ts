8c03c18bb7f6401c887df469e276ce88897dc4fa
K. Jetnipit <jetnipit54@gmail.com>
2023-02-16 14:49:05 +0700

    refactor: add comment length validator

--- apps/backend/src/review/dto/CreateReview.dto.ts
+++ apps/backend/src/review/dto/CreateReview.dto.ts
@@ -1,11 +1,15 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
 
 export class CreateReviewDto {
   @IsNotEmpty()
   guideID: string;
+
   @IsNotEmpty()
   touristID: string;
+
   @IsNumber()
   rating: number;
+
+  @MaxLength(200)
   comment: string;
 }

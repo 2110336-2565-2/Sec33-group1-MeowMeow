386efe7dc3dbeb59d605c34829d551543c48df55
K. Jetnipit <jetnipit54@gmail.com>
2023-02-18 00:39:10 +0700

    feat: add auth guard for review and guide module

--- apps/backend/src/guide/guide.module.ts
+++ apps/backend/src/guide/guide.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { GuidesController } from './guide.controller';
 import { GuideServiceImpl, GuideService } from './guide.service';
 import { GuideRepository } from './guide.repository';
+import { AuthMiddleware } from 'src/common/middleware/auth.middleware';
+import { AuthServiceImpl } from 'src/auth/auth.service';
+import { UserRepository } from 'src/user/user.repository';
 
 @Module({
   controllers: [GuidesController],
@@ -11,8 +14,19 @@ import { GuideRepository } from './guide.repository';
       provide: 'GuideService',
       useClass: GuideServiceImpl,
     },
+    {
+      provide: 'AuthService',
+      useClass: AuthServiceImpl,
+    },
+    UserRepository,
     GuideRepository,
     PrismaService,
   ],
 })
-export class GuidesModule {}
+export class GuidesModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: '/guides/**', method: RequestMethod.ALL });
+  }
+}
--- apps/backend/src/review/review.module.ts
+++ apps/backend/src/review/review.module.ts
@@ -1,8 +1,11 @@
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ReviewController } from './review.controller';
 import { ReviewServiceImpl } from './review.service';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ReviewRepository } from './review.repository';
+import { AuthMiddleware } from 'src/common/middleware/auth.middleware';
+import { AuthServiceImpl } from 'src/auth/auth.service';
+import { UserRepository } from 'src/user/user.repository';
 
 @Module({
   controllers: [ReviewController],
@@ -11,8 +14,19 @@ import { ReviewRepository } from './review.repository';
       provide: 'ReviewService',
       useClass: ReviewServiceImpl,
     },
+    {
+      provide: 'AuthService',
+      useClass: AuthServiceImpl,
+    },
+    UserRepository,
     ReviewRepository,
     PrismaService,
   ],
 })
-export class ReviewModule {}
+export class ReviewModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: '/reviews/**', method: RequestMethod.ALL });
+  }
+}

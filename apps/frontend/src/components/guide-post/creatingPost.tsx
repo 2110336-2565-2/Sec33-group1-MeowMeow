c486e53999d4570658e997e41840491fc0e19810
TikhampornSky <tikhamporntan@gmail.com>
2023-02-20 18:04:00 +0700

    fix: post form page fix: bug rebase

--- apps/frontend/src/components/guide-post/creatingPost.tsx
+++ apps/frontend/src/components/guide-post/creatingPost.tsx
@@ -1,28 +1,16 @@
 import { Typography } from "@mui/material";
 import React from "react";
-<<<<<<< HEAD
 import PostContainer from "./postContainer";
 import PostForm from "./postForm";
-=======
-import GuidePostForm from "./form";
-import PostContainer from "./postContainer";
->>>>>>> 9f25609 (fix: init form-post)
 
 export default function CreateingPost() {
   return (
     <>
       <PostContainer>
         <Typography variant="h5" fontWeight="600" marginBottom={"40px"}>
-<<<<<<< HEAD
           Create Post
         </Typography>
         <PostForm methodType="POST" />
-=======
-          {" "}
-          Create Post{" "}
-        </Typography>
-        <GuidePostForm />
->>>>>>> 9f25609 (fix: init form-post)
       </PostContainer>
     </>
   );
--- apps/frontend/src/pages/guide-post/index.tsx
+++ apps/frontend/src/pages/guide-post/index.tsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Button, ButtonGroup } from "@mui/material";
 import { Router, useRouter } from "next/router";
 import React from "react";
@@ -37,15 +38,47 @@ export default function GuidePost() {
 }
 =======
 import CreateingPost from "@/components/guide-post/creatingPost";
+=======
+import { Button, ButtonGroup } from "@mui/material";
+import { Router, useRouter } from "next/router";
+>>>>>>> 3b5c4ab (fix: post form page)
 import React from "react";
 
-function GuidePost() {
+// For testing purposes, I have created a button group that will allow you to navigate to the create and edit pages.
+export default function GuidePost() {
+  const router = useRouter();
+  const onChangeCreate = () => {
+    router.push("guide-post/create");
+  };
+  const onChangeEdit = () => {
+    router.push("guide-post/edit");
+  };
+  const onChangeDelete = () => {
+    console.log("delete");
+  };
+
+  const buttons = [
+    <Button key="one" onClick={onChangeCreate}>
+      Create
+    </Button>,
+    <Button key="two" onClick={onChangeEdit}>
+      Edit
+    </Button>,
+    <Button key="three" onClick={onChangeDelete}>
+      Delete
+    </Button>,
+  ];
   return (
     <>
-      <CreateingPost />
+      <ButtonGroup size="large" aria-label="large button group">
+        {buttons}
+      </ButtonGroup>
     </>
   );
 }
+<<<<<<< HEAD
 
 export default GuidePost;
 >>>>>>> 9f25609 (fix: init form-post)
+=======
+>>>>>>> 3b5c4ab (fix: post form page)

7280ab29f9b3a70c289292b6fd15f5b86aa887b5
Polapob <71330294+Polapob@users.noreply.github.com>
2023-02-21 13:11:40 +0700

    feat: add refresh access token in axios interceptors

--- apps/frontend/src/utils/interceptor.ts
+++ apps/frontend/src/utils/interceptor.ts
@@ -1,13 +1,23 @@
 import { AxiosError, AxiosResponse, isAxiosError } from "axios";
+import apiClient from "./apiClient";
 
 export const responseSuccess = (response: AxiosResponse) => {
   return response;
 };
 
-export const responseReject = (error: Error) => {
+export const responseReject = async (error: Error & { config: any }) => {
+  const config = error?.config;
   if (isAxiosError(error)) {
     const err: AxiosError<{ message: string }> = error;
-    const { response } = err;
+    const { response, status } = err;
+    if (
+      response?.status === 401 &&
+      response.data.message === "access token is expired"
+    ) {
+      // TODO refresh access token after access token expired
+      await apiClient.post("/auth/refresh");
+      return await apiClient(config);
+    }
     if (response?.data?.message) {
       err.message = response.data.message;
       err.status = response.status;

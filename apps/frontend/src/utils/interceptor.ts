53db64beeb12de2da19810571aba8462237fafac
Polapob <71330294+Polapob@users.noreply.github.com>
2023-02-21 13:15:13 +0700

    fix: get config from axios error

--- apps/frontend/src/utils/interceptor.ts
+++ apps/frontend/src/utils/interceptor.ts
@@ -5,18 +5,19 @@ export const responseSuccess = (response: AxiosResponse) => {
   return response;
 };
 
-export const responseReject = async (error: Error & { config: any }) => {
-  const config = error?.config;
+export const responseReject = async (error: Error) => {
   if (isAxiosError(error)) {
     const err: AxiosError<{ message: string }> = error;
-    const { response, status } = err;
+    const { response, config } = err;
     if (
       response?.status === 401 &&
       response.data.message === "access token is expired"
     ) {
       // TODO refresh access token after access token expired
       await apiClient.post("/auth/refresh");
-      return await apiClient(config);
+      if (config) {
+        return await apiClient(config);
+      }
     }
     if (response?.data?.message) {
       err.message = response.data.message;

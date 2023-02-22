2c818dfe688811903c13d5976f2c6e099f9b4fce
K. Jetnipit <jetnipit54@gmail.com>
2023-02-22 22:19:25 +0700

    chore: rename config

--- packages/config/src/backend.config.ts
+++ packages/config/src/backend.config.ts
@@ -3,7 +3,7 @@
 export type BackendConfig = {
   port: number;
   bcrypt: {
-    salt: number;
+    hashRound: number;
   };
   jwt: {
     secret: string;
@@ -23,12 +23,13 @@ export type BackendConfig = {
 export const loadBackendConfig = (): BackendConfig => ({
   port: parseInt(process.env.BACKEND_PORT ?? "", 10) || 3000,
   bcrypt: {
-    salt: Number(process.env.BACKEND_BCRYPT_SALT) ?? 10,
+    hashRound: parseInt(process.env.BACKEND_BCRYPT_HASH_ROUND ?? "", 10) || 10,
   },
   jwt: {
     secret: process.env.BACKEND_JWT_SECRET ?? "xxxx-9ababe0jge9j",
-    expire: Number(process.env.BACKEND_JWT_EXPIRE) ?? 600,
-    refreshExpire: Number(process.env.BACKEND_REFRESH_TOKEN_EXPIRE) ?? 864000,
+    expire: parseInt(process.env.BACKEND_JWT_EXPIRE ?? "", 10) || 600,
+    refreshExpire:
+      parseInt(process.env.BACKEND_REFRESH_TOKEN_EXPIRE ?? "", 10) || 864000,
   },
   swagger: {
     enable: process.env.BACKEND_SWAGGER_ENABLE === "true",

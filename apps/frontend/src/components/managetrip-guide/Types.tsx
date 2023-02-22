a83d22c3c140ec9ce2fe0fb6ee42107cee397ea2
TikhampornSky <tikhamporntan@gmail.com>
2023-02-22 13:38:24 +0700

    fix: rebase

--- apps/frontend/src/components/managetrip-guide/Types.tsx
+++ apps/frontend/src/components/managetrip-guide/Types.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 type Request = {
   id: number;
   customerName: string;
@@ -26,3 +27,34 @@ export interface IAcceptedRequestProps {
   cancelledRequests: Request[];
   handleCancel: (request: Request) => void;
 }
+||||||| parent of f2437d0 (fix: move landing page to home page)
+=======
+type Request = {
+    id: number,
+    customerName: string,
+    tripName: string,
+    startDate: string,
+    endDate: string,
+    price:number,
+    numCustomer:number
+    status:number
+  };
+export default Request;
+export interface TabPanelProps {
+    children?: React.ReactNode,
+    index: number,
+    value: number
+}
+export interface INewRequestProps {
+    requests:Request[],
+    confirmedRequests:Request[],
+    cancelledRequests:Request[],
+    handleConfirm: (request:Request)=>void,
+    handleCancel:(request:Request)=>void
+}
+export interface IAcceptedRequestProps {
+    confirmedRequests:Request[],
+    cancelledRequests:Request[],
+    handleCancel:(request:Request)=>void
+}
+>>>>>>> f2437d0 (fix: move landing page to home page)
--- apps/frontend/src/pages/managetrip-guide.tsx
+++ apps/frontend/src/pages/managetrip-guide.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import Navbar from "@/components/common/Navbar";
 import { Box } from "@mui/material";
@@ -15,3 +16,23 @@ const ManageTripGuide = () => {
 };
 
 export default ManageTripGuide;
+||||||| parent of f2437d0 (fix: move landing page to home page)
+=======
+import React from "react";
+import Navbar from "@/components/common/Navbar";
+import { Box } from "@mui/material";
+import Content from "@/components/managetrip-guide/Content";
+
+const ManageTripGuide = () => {
+  return(
+    <>
+    <Navbar/>
+    <Box sx={{ display: "flex", justifyContent: "center", paddingY:"5vh" }}>
+        <Content></Content>
+      </Box>
+    </>
+  )
+};
+
+export default ManageTripGuide;
+>>>>>>> f2437d0 (fix: move landing page to home page)

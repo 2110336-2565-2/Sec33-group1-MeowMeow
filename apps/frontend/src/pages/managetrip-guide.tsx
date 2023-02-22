f2437d014550013ee6ac6be9fb71cd8e0ce82e0b
TikhampornSky <tikhamporntan@gmail.com>
2023-02-22 13:38:24 +0700

    fix: move landing page to home page

---
+++ apps/frontend/src/pages/managetrip-guide.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "@/components/common/Navbar";
+import { Box } from "@mui/material";
+import Content from "@/components/managetrip-guide/Content";
+
+const ManageTripGuide = () => {
+  return(
+    <>
+    <Navbar/>
+    <Box sx={{ display: "flex", justifyContent: "center", paddingY:"5vh" }}>
+        <Content></Content>
+      </Box>
+    </>
+  )
+};
+
+export default ManageTripGuide;

286baea15121c2c4308c60b1738a12b87a3f9b5b
Polapob <71330294+Polapob@users.noreply.github.com>
2023-02-23 17:52:54 +0700

    fix: add axios interceptor condition when retry and get 401 status again

--- apps/frontend/src/utils/interceptor.ts
+++ apps/frontend/src/utils/interceptor.ts
@@ -9,14 +9,14 @@ export const responseReject = async (error: Error) => {
   if (isAxiosError(error)) {
     const err: AxiosError<{ message: string }> = error;
     const { response, config } = err;
-    if (
-      response?.status === 401 &&
-      response.data.message === "access token is expired"
-    ) {
-      // TODO refresh access token after access token expired
-      await apiClient.post("/auth/refresh");
-      if (config) {
+
+    if (response?.status === 401) {
+      if (config && config.url !== "/auth/refresh") {
+        await apiClient.post("/auth/refresh");
         return await apiClient(config);
+      }
+      if (config && config.url === "/auth/refresh") {
+        window.location.href = "/login";
       }
     }
     if (response?.data?.message) {

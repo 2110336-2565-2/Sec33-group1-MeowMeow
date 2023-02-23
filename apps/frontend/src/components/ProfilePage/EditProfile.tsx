19438569736abece043051a90b0864b5869e3b92
Polapob <71330294+Polapob@users.noreply.github.com>
2023-02-23 19:12:23 +0700

    feat: implement upload image logic

--- apps/frontend/src/components/ProfilePage/EditProfile.tsx
+++ apps/frontend/src/components/ProfilePage/EditProfile.tsx
@@ -1,23 +1,48 @@
 import { PROFILE_INPUT_IDs } from "@/constants/ProfilePage";
+import { NotificationContext } from "@/context/NotificationContext";
+import apiClient from "@/utils/apiClient";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import { FormEventHandler } from "react";
+import {
+  ChangeEventHandler,
+  FormEventHandler,
+  useContext,
+  useState,
+} from "react";
 import StyleTextField from "../LoginPage/StyledTextField";
 import { IProfileData } from "./types/profilePage";
+import useCustomSnackbar from "@/hooks/useCustomSnackbar";
+import AppSnackbar from "../common/AppSnackbar";
 
 interface IEditProfileProps {
   profileData: IProfileData;
 }
 
 const EditProfile = ({ profileData }: IEditProfileProps) => {
-  const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+  const [image, setImage] = useState<File | undefined>(undefined);
+  const { addNotification } = useContext(NotificationContext);
+  const { onClose, onExit, isOpen, messageInfo } = useCustomSnackbar();
+  const onUploadImage: ChangeEventHandler<HTMLInputElement> = (event) => {
+    if (!event.target.files) {
+      return;
+    }
+    const fileUploaded = event.target.files[0];
+    setImage(fileUploaded);
+  };
+  const onSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
     const formBody = PROFILE_INPUT_IDs.reduce((prev, formId) => {
       prev[formId] = event.currentTarget[formId].value;
       return prev;
     }, {} as { [key: string]: string });
-    console.log(formBody);
+
+    try {
+      await apiClient.put("/users/users", formBody);
+    } catch (err) {
+      const error = err as Error;
+      addNotification(error.message, "error");
+    }
   };
 
   return (
@@ -52,6 +77,54 @@ const EditProfile = ({ profileData }: IEditProfileProps) => {
         defaultValue={profileData.email}
         id="email"
       />
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        sx={{ width: "100%" }}
+      >
+        <Typography variant="body2" fontWeight="500">
+          Upload Profile Image:
+        </Typography>
+        <Stack direction="row" alignItems="center" spacing="1rem">
+          <Button variant="contained" color="secondary" component="label">
+            <Typography
+              variant="body2"
+              fontWeight="500"
+              color="white"
+              textTransform="none"
+            >
+              Upload Image
+            </Typography>
+            <input
+              hidden
+              accept="/images/*"
+              type="file"
+              onChange={onUploadImage}
+            />
+          </Button>
+          <Typography
+            variant="body2"
+            sx={{
+              cursor: "pointer",
+              ":hover": {
+                color: "#F46D21",
+                transition: "color 250ms",
+              },
+            }}
+            onClick={() => {
+              if (!image) {
+                return;
+              }
+              const objectURL = URL.createObjectURL(image);
+              window.open(objectURL);
+            }}
+          >
+            {image && image?.name.toString()}
+          </Typography>
+        </Stack>
+      </Stack>
+
       <Button type="submit" variant="contained" fullWidth>
         <Typography
           variant="body2"
@@ -63,6 +136,12 @@ const EditProfile = ({ profileData }: IEditProfileProps) => {
           Save Profile
         </Typography>
       </Button>
+      <AppSnackbar
+        messageInfo={messageInfo}
+        isOpen={isOpen}
+        onClose={onClose}
+        onExit={onExit}
+      />
     </Stack>
   );
 };

026abe4018ff1dce29cb8a1d5a8026010014539d
Polapob <71330294+Polapob@users.noreply.github.com>
2023-02-27 21:04:51 +0700

    fix: change publicEnv to env in next.config

--- apps/frontend/src/utils/apiClient.ts
+++ apps/frontend/src/utils/apiClient.ts
@@ -1,11 +1,9 @@
 import axios, { CreateAxiosDefaults } from "axios";
-import getConfig from "next/config";
 import { responseReject, responseSuccess } from "./interceptor";
-const { publicRuntimeConfig } = getConfig();
 
 const createAxiosConfig = (): CreateAxiosDefaults<any> => {
   return {
-    baseURL: publicRuntimeConfig.backendBaseURL,
+    baseURL: process.env.backendBaseURL,
     withCredentials: true,
   };
 };

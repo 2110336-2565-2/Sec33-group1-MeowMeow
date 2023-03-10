4c3054592dfcb5eec0b4795b02e04da3fef06a97
K. Jetnipit <jetnipit54@gmail.com>
2023-03-10 17:48:35 +0700

    feat: add media service and controller

--- packages/types/src/dtos/media/download.dto.ts
+++ packages/types/src/dtos/media/download.dto.ts
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-=======
-import { Stream } from "stream";
-
->>>>>>> 43bfa67... feat: add scaffolding for media module
 export class DownloadRequest {
   id: string;
 }
 
 export class DownloadResponse {
   message: string;
-<<<<<<< HEAD
   file: Buffer;
-=======
-  file: Stream;
->>>>>>> 43bfa67... feat: add scaffolding for media module
 }

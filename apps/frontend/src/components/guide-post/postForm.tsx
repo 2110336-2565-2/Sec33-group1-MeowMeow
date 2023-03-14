5664d428394f4034a8485e3ebcda77b6298ebd44
TikhampornSky <tikhamporntan@gmail.com>
2023-03-14 13:23:28 +0700

    fix: remove unused stage

--- apps/frontend/src/components/guide-post/postForm.tsx
+++ apps/frontend/src/components/guide-post/postForm.tsx
@@ -6,7 +6,6 @@ import {
   Snackbar,
   Typography,
 } from "@mui/material";
-import dayjs, { Dayjs } from "dayjs";
 import React from "react";
 import usePostForm from "@/hooks/usePostForm";
 import useCustomSnackbar from "@/hooks/useCustomSnackbar";
@@ -17,18 +16,6 @@ export interface IPostForm {
 }
 
 export default function PostForm({ methodType }: IPostForm) {
-  const [endDate, setEndDate] = React.useState<Dayjs | null>(dayjs(undefined));
-  const [startDate, setStartDate] = React.useState<Dayjs | null>(
-    dayjs(undefined)
-  );
-
-  const handleEndChange = (newValue: Dayjs | null) => {
-    setEndDate(newValue);
-  };
-  const handleStartChange = (newValue: Dayjs | null) => {
-    setStartDate(newValue);
-  };
-
   const { onClose, onExit, isOpen, messageInfo } = useCustomSnackbar();
   const { onSubmit, isLoading } = usePostForm({
     methodType: methodType,

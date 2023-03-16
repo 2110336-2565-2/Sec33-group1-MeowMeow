a334679e9aadc3244ed2f2059f08d8df9aa2556b
K. Jetnipit <jetnipit54@gmail.com>
2023-03-16 22:12:22 +0700

    docs: add Swagger docs for media module

--- apps/backend/src/media/media.controller.ts
+++ apps/backend/src/media/media.controller.ts
@@ -12,17 +12,53 @@ import {
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { AccountMetadata } from 'types';
+import {
+  AccountMetadata,
+  DownloadRequest,
+  DownloadResponse,
+  UploadRequest,
+  UploadResponse,
+} from 'types';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { MediaService } from './media.service';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('media')
 @Controller('media')
 export class MediaController {
   constructor(
     @Inject('MediaService') private readonly mediaService: MediaService,
   ) {}
 
+  @ApiCookieAuth('access_token')
+  @ApiOperation({
+    summary: 'upload a file',
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    type: UploadRequest,
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'successfully uploaded a file',
+    type: UploadResponse,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'valid session is not provided',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'internal server error',
+  })
   @UseGuards(AuthGuard)
   @UseInterceptors(FileInterceptor('file'))
   @Post()
@@ -42,6 +78,30 @@ export class MediaController {
     }
   }
 
+  @ApiCookieAuth('access_token')
+  @ApiOperation({
+    summary: 'decline booking by ID',
+  })
+  @ApiBody({
+    type: DownloadRequest,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'successfully downloaded a file',
+    type: DownloadResponse,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'valid session is not provided',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'booking with given ID was not found',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'internal server error',
+  })
   @UseGuards(AuthGuard)
   @Get(':id')
   async download(@Param('id') id: string, @Res({ passthrough: true }) res) {
--- packages/types/src/dtos/media/download.dto.ts
+++ packages/types/src/dtos/media/download.dto.ts
@@ -1,8 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsUUID } from "class-validator";
+
 export class DownloadRequest {
+  @ApiProperty({
+    type: () => String,
+    example: "9a5bb8cc-2af9-4057-8153-eaa8d4a2b0d1",
+  })
+  @IsUUID(4)
   id: string;
 }
 
 export class DownloadResponse {
+  @ApiProperty({
+    type: () => String,
+    example: "success",
+  })
   message: string;
+
+  @ApiProperty({
+    type: () => String,
+    format: "binary",
+  })
   file: Buffer;
 }

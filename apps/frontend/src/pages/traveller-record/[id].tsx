274a3a19aa2857a5f3fbd984811d6176fad4af48
Tikhamporn Tepsut <88883551+TikhampornSky@users.noreply.github.com>
2023-03-18 15:56:09 +0700

    fix: import error (#66)

---
+++ apps/frontend/src/pages/traveller-record/[id].tsx
@@ -0,0 +1,10 @@
+import TravellerRecordComponent from "@/components/traveller-record";
+import React from "react";
+
+export default function TravellerRecord() {
+  return (
+    <>
+      <TravellerRecordComponent />
+    </>
+  );
+}

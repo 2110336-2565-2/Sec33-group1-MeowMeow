a986a0904d92e255e15fd17c3e4e29731e7f42cd
TikhampornSky <tikhamporntan@gmail.com>
2023-03-19 19:50:32 +0700

    fix: init payment-admin

---
+++ apps/frontend/src/components/payment-admin/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TableRecord from "./tableShow/table";
+
+export default function PaymentAdminComponent() {
+  return (
+    <>
+      <TableRecord />
+    </>
+  );
+}

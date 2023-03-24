b9a5e43b0fc1785ef102a635390b9d4bd3d58a7c
K. Jetnipit <jetnipit54@gmail.com>
2023-03-24 14:44:08 +0700

    feat: create guide registration API

--- apps/backend/src/guides/guides.controller.ts
+++ apps/backend/src/guides/guides.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Get,
   HttpException,
@@ -6,12 +7,19 @@ import {
   Inject,
   NotFoundException,
   Param,
+  ParseFilePipe,
+  Post,
   Query,
+  Req,
   Res,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import {
+  AccountMetadata,
   GetGuideByIdRequest,
   GetGuideReviewsRequest,
   SearchGuidesRequest,
@@ -19,15 +27,26 @@ import {
 } from 'types';
 import { GuidesService } from './guides.service';
 import {
+  ApiBody,
+  ApiConsumes,
   ApiCookieAuth,
   ApiOperation,
   ApiParam,
   ApiQuery,
   ApiResponse,
+  ApiTags,
 } from '@nestjs/swagger';
 import { Response } from 'express';
 import { ReviewsService } from 'src/reviews/reviews.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileIsDefinedValidator } from 'src/common/file.validator';
+import {
+  FailedRelationConstraintError,
+  RecordAlreadyExist,
+} from './guides.common';
 
+@ApiTags('guides')
 @Controller('guides')
 export class GuidesController {
   constructor(
@@ -117,6 +136,71 @@ export class GuidesController {
     } catch (e) {
       console.log(e);
       throw e;
+    }
+  }
+
+  @ApiCookieAuth('access_token')
+  @ApiOperation({ summary: 'register for a guide' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        certificate: {
+          description: 'certificate file in JPEG/PNG format',
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'successfully registered for a guide',
+    type: SearchGuidesResponse,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'access token not provided',
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'user has already been a guide',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'internal server error',
+  })
+  @UseInterceptors(FileInterceptor('certificate'))
+  @UseGuards(AuthGuard)
+  @Post('registration/self')
+  async registerForGuide(
+    @Req() req,
+    @UploadedFile(
+      new ParseFilePipe({ validators: [new FileIsDefinedValidator()] }),
+    )
+    certificate: Express.Multer.File,
+    @Res({ passthrough: true }) res,
+  ) {
+    try {
+      const account: AccountMetadata = req.account;
+      const resBody = await this.guidesService.registerUserForGuide({
+        userId: account.userId,
+        certificate: certificate.buffer,
+      });
+      res.status(HttpStatus.CREATED).send(resBody);
+    } catch (e) {
+      console.log(e);
+      if (e instanceof RecordAlreadyExist) {
+        throw new HttpException(
+          'user has already been a guide',
+          HttpStatus.CONFLICT,
+        );
+      }
+      throw new HttpException(
+        'internal server errror',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
--- apps/backend/src/guides/guides.module.ts
+++ apps/backend/src/guides/guides.module.ts
@@ -1,12 +1,14 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { GuidesController } from './guides.controller';
-import { GuidesServiceImpl, GuidesService } from './guides.service';
+import { GuidesServiceImpl } from './guides.service';
 import { GuidesRepository } from './guides.repository';
 import { AuthMiddleware } from 'src/common/middleware/auth.middleware';
 import { AuthServiceImpl } from 'src/auth/auth.service';
 import { UsersRepository } from 'src/users/users.repository';
 import { ReviewsModule } from 'src/reviews/reviews.module';
+import { MediaServiceImpl } from 'src/media/media.service';
+import { MediaModule } from 'src/media/media.module';
 
 @Module({
   controllers: [GuidesController],
@@ -23,7 +25,7 @@ import { ReviewsModule } from 'src/reviews/reviews.module';
     GuidesRepository,
     PrismaService,
   ],
-  imports: [ReviewsModule],
+  imports: [ReviewsModule, MediaModule],
 })
 export class GuidesModule {
   configure(consumer: MiddlewareConsumer) {

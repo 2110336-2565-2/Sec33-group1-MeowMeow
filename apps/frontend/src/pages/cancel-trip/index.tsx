6b8bd995b525ffe9b31f9464c6c8870dfb490794
TikhampornSky <tikhamporntan@gmail.com>
2023-03-24 17:05:00 +0700

    fix: rebase

---
+++ apps/frontend/src/pages/cancel-trip/index.tsx
@@ -0,0 +1,10 @@
+import CancelTripComponent from "@/components/traveller-record/cancel-trip";
+import React from "react";
+
+export default function CancelTrip() {
+  return (
+    <>
+      <CancelTripComponent />
+    </>
+  );
+}

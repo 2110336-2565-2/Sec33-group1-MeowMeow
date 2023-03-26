1290cb03f5f8eb561e3a8886e0ac98bd628d9d3e
Polapob <71330294+Polapob@users.noreply.github.com>
2023-03-26 21:56:01 +0700

    fix: add sample authProvider

--- apps/frontend/src/pages/managetrip-guide.tsx
+++ apps/frontend/src/pages/managetrip-guide.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import Navbar from "@/components/common/Navbar";
 import { Box } from "@mui/material";
 import Content from "@/components/managetrip-guide/Content";
+import AuthProvider from "@/context/AuthContext";
 
 const ManageTripGuide = () => {
   return (
-    <>
-      <Navbar />
-      <Box sx={{ display: "flex", justifyContent: "center", paddingY: "5vh" }}>
-        <Content></Content>
-      </Box>
-    </>
+    <AuthProvider roleAllowed={["GUIDE"]}>
+      <>
+        <Navbar />
+        <Box
+          sx={{ display: "flex", justifyContent: "center", paddingY: "5vh" }}
+        >
+          <Content></Content>
+        </Box>
+      </>
+    </AuthProvider>
   );
 };
 

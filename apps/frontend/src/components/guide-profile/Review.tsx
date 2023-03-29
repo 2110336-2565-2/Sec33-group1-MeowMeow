558c029963abfdbf619cefef86c8ef236233ebbb
Pacharapon Arpanantikul <pacharaponarp@gmail.com>
2023-03-29 13:42:31 +0700

    feat: connect guide profile page to api

--- apps/frontend/src/components/guide-profile/Review.tsx
+++ apps/frontend/src/components/guide-profile/Review.tsx
@@ -1,61 +1,94 @@
 import { Grid, Rating } from "@mui/material";
 import StarIcon from "@mui/icons-material/Star";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import apiClient from "@/utils/apiClient";
 interface IReviewProps {
   rating: number;
   timestamp: string;
-  reviewer: string;
+  reviewerId: string;
   detail: string;
+  newLimit: () => void;
+  isLast: boolean;
 }
+const getReviewer = async (id: string) => {
+  const response = await apiClient.get("users/" + id);
+  return response.data.firstName + " " + response.data.lastName;
+};
 export default function Review({
   rating,
   timestamp,
-  reviewer,
+  reviewerId,
   detail,
+  newLimit,
+  isLast,
 }: IReviewProps) {
   dayjs.extend(relativeTime).locale(timestamp);
   var timeago = dayjs(timestamp).fromNow();
 
+  const [reviewer, setReviewer] = useState("name");
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      if (!reviewerId) return;
+      const data = await getReviewer(reviewerId as string);
+      setReviewer(data);
+    };
+    fetchUserProfile();
+  }, [reviewerId]);
+
+  const reviewRef = useRef(null);
+  useEffect(() => {
+    if (!reviewRef?.current) return;
+    const observer = new IntersectionObserver(([entry]) => {
+      if (isLast && entry.isIntersecting) {
+        newLimit();
+        observer.unobserve(entry.target);
+      }
+    });
+
+    observer.observe(reviewRef.current);
+  }, [isLast]);
   return (
-    <Grid
-      container
-      direction="column"
-      bgcolor="#F3F4F6"
-      borderRadius={5}
-      color="#475569"
-      fontFamily="Inter"
-      fontWeight={400}
-      fontSize={{ xs: 12, sm: 14, md: 16 }}
-      lineHeight={{ xs: 1.5, md: 2 }}
-      padding={{ xs: "6vw", sm: "4vw", md: "3vw" }}
-      marginBottom="3vh"
-    >
-      <Grid container direction="row">
-        <Grid item marginRight="auto">
-          <Rating
-            value={rating}
-            readOnly
-            precision={0.5}
-            icon={<StarIcon sx={{ fontSize: { xs: 14, sm: 20, md: 28 } }} />}
-            emptyIcon={
-              <StarIcon
-                sx={{ opacity: 1.0, fontSize: { xs: 14, sm: 20, md: 28 } }}
-              />
-            }
-          />
+    <div ref={reviewRef}>
+      <Grid
+        container
+        direction="column"
+        bgcolor="#F3F4F6"
+        borderRadius={5}
+        color="#475569"
+        fontFamily="Inter"
+        fontWeight={400}
+        fontSize={{ xs: 12, sm: 14, md: 16 }}
+        lineHeight={{ xs: 1.5, md: 2 }}
+        padding={{ xs: "6vw", sm: "4vw", md: "3vw" }}
+        marginBottom="3vh"
+      >
+        <Grid container direction="row">
+          <Grid item marginRight="auto">
+            <Rating
+              value={rating}
+              readOnly
+              precision={0.5}
+              icon={<StarIcon sx={{ fontSize: { xs: 14, sm: 20, md: 28 } }} />}
+              emptyIcon={
+                <StarIcon
+                  sx={{ opacity: 1.0, fontSize: { xs: 14, sm: 20, md: 28 } }}
+                />
+              }
+            />
+          </Grid>
+          <Grid item paddingRight={{ xs: "3vw", sm: "5vw", md: "3vw" }}>
+            {" "}
+            {timeago}
+          </Grid>
+          <Grid item paddingRight="1.5vw">
+            {" "}
+            {reviewer}
+          </Grid>
         </Grid>
-        <Grid item paddingRight={{ xs: "3vw", sm: "5vw", md: "3vw" }}>
-          {" "}
-          {timeago}
-        </Grid>
-        <Grid item paddingRight="1.5vw">
-          {" "}
-          {reviewer}
-        </Grid>
+        <Grid item>{detail}</Grid>
       </Grid>
-      <Grid item>{detail}</Grid>
-    </Grid>
+    </div>
   );
 }
--- apps/frontend/src/pages/guide-profile/[id].tsx
+++ apps/frontend/src/pages/guide-profile/[id].tsx
@@ -1,17 +1,26 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "@/components/common/Navbar";
 import Content from "@/components/guide-profile/Content";
 import { Box } from "@mui/material";
+import AuthProvider from "@/context/AuthContext";
+import apiClient from "@/utils/apiClient";
 interface IReview {
-  rating: number;
-  timestamp: string;
-  reviewer: string;
-  detail: string;
+  guideId: number;
+  reviewId: number;
+  reviewerId: string;
+  score: number;
+  publishDate: string;
+  text: string;
 }
-const name = "John Smith";
-const desc =
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione magni ipsa deleniti delectus nobis provident asperiores neque amet rerum modi adipisci possimus repellendus distinctio doloremque quasi sit, quis reiciendis iste?";
+interface IGuide {
+  guideId: number;
+  userId: number;
+  firstName: string;
+  lastName: string;
+  certificateId: string;
+  averageReviewScore: number;
+}
 const imageurl =
   "https://images.unsplash.com/photo-1506439773649-6e0eb8cfb237?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
 const tagsTourStyle = [
@@ -34,42 +43,82 @@ const tagsLocation = [
 ];
 const accept = 60;
 const total = 100;
-const reviewCount: number[] = [8, 4, 2, 3, 1]; //5 to 1
-const reviews: IReview[] = [
-  {
-    rating: 4.5,
-    timestamp: "2023-02-10T:12:00:00",
-    reviewer: "pasta spaghetti",
-    detail:
-      "potato tomato origano cocomo potato tomato origano cocomo potato tomato origano cocomo potato tomato origano cocomo potato tomato origano cocomo potato tomato origano cocomo",
-  },
-  {
-    rating: 2,
-    timestamp: "2023-01-10T:12:00:00",
-    reviewer: "Alice Bob",
-    detail:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim deleniti velit beatae. Officia reprehenderit sunt fugiat iusto consequuntur! Dolore quia minus, natus incidunt provident laudantium. Ipsum sunt sit libero cumque.",
-  },
-];
+// const reviewCount: number[] = [8, 4, 2, 3, 1]; //5 to 1
+// const reviews: IReview[] = [
+//   {
+//     rating: 4.5,
+//     timestamp: "2023-02-10T:12:00:00",
+//     reviewer: "pasta spaghetti",
+//     detail:
+//       "potato tomato origano cocomo potato tomato origano cocomo potato tomato origano cocomo potato tomato origano cocomo potato tomato origano cocomo potato tomato origano cocomo",
+//   },
+//   {
+//     rating: 2,
+//     timestamp: "2023-01-10T:12:00:00",
+//     reviewer: "Alice Bob",
+//     detail:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim deleniti velit beatae. Officia reprehenderit sunt fugiat iusto consequuntur! Dolore quia minus, natus incidunt provident laudantium. Ipsum sunt sit libero cumque.",
+//   },
+// ];
+const getData = async (id: string) => {
+  const response = await apiClient.get("guides/" + id);
+  return response;
+};
+const getReview = async (id: string, page: number) => {
+  const response = await apiClient.get("guides/" + id + "/reviews/" + page);
+  return response;
+};
 export default function GuideProfile() {
+  const [guideData, setGuideData] = useState<IGuide>({
+    guideId: 1,
+    userId: 1,
+    firstName: "name",
+    lastName: "lastname",
+    certificateId: "aaaaaaaab",
+    averageReviewScore: 3,
+  });
+  const [reviews, setReviews] = useState<IReview[]>([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const { id } = router.query;
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      if (!id) return;
+      const data = await getData(id as string);
+      setGuideData(data.data);
+    };
+    fetchUserProfile();
+  }, [id]);
+  useEffect(() => {
+    const fetchReviews = async () => {
+      if (!id) return;
+      setLoading(true);
+      const data = await getReview(id as string, page as number);
+      setReviews([...reviews, ...data.data]);
+      setLoading(false);
+    };
+    fetchReviews();
+  }, [id, page]);
+  const loadMoreItems = async () => setPage(page + 1);
   return (
-    <>
+    <AuthProvider roleAllowed={["USER"]}>
       <Navbar />
       <Box sx={{ display: "flex", justifyContent: "center" }}>
         <Content
-          name={name}
-          desc={desc}
+          name={guideData.firstName + " " + guideData.lastName}
+          certificateId={guideData.certificateId}
           imageurl={imageurl}
           tagsTourStyle={tagsTourStyle}
           tagsLocation={tagsLocation}
           accept={accept}
           total={total}
-          reviewCount={reviewCount}
+          reviewCount={[1, 2, 3, 4, 5]}
           reviews={reviews}
+          averageReviewScore={guideData.averageReviewScore}
+          newLimit={() => setPage(page + 1)}
         ></Content>
       </Box>
-    </>
+    </AuthProvider>
   );
 }

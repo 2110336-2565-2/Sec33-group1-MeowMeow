f7bd91a5a86a9c31774ebf9d0b33837dadf871eb
Tikhamporn Tepsut <88883551+TikhampornSky@users.noreply.github.com>
2023-03-30 10:22:24 +0700

    Frontend register guide (#97)
    * fix: fix rebase bugs2
    
    * fix: fix rebase bugs3
    
    * fix: next env
    
    * fix: next config env

--- apps/frontend/src/components/guide-register/form.tsx
+++ apps/frontend/src/components/guide-register/form.tsx
@@ -27,7 +27,6 @@ export default function GuideRegisterForm() {
     const fileUploaded = event.target.files[0];
     setImage(fileUploaded);
   };
-
   const [location, setLocation] = React.useState<readonly ChipData[]>([
     { key: 0, label: "Bangkok" },
     { key: 1, label: "Thailand" },

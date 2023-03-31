815179ad7f5ac29e86e29c325a321a7de22c9de1
Pacharapon Arpanantikul <pacharaponarp@gmail.com>
2023-03-31 20:49:30 +0700

    fix: something

--- apps/frontend/src/pages/managetrip-guide.tsx
+++ apps/frontend/src/pages/managetrip-guide.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Navbar from "@/components/common/Navbar";
 import { Box } from "@mui/material";
@@ -21,23 +20,3 @@ const ManageTripGuide = () => {
 };
 
 export default ManageTripGuide;
-||||||| parent of f2437d0 (fix: move landing page to home page)
-=======
-import React from "react";
-import Navbar from "@/components/common/Navbar";
-import { Box } from "@mui/material";
-import Content from "@/components/managetrip-guide/Content";
-
-const ManageTripGuide = () => {
-  return(
-    <>
-    <Navbar/>
-    <Box sx={{ display: "flex", justifyContent: "center", paddingY:"5vh" }}>
-        <Content></Content>
-      </Box>
-    </>
-  )
-};
-
-export default ManageTripGuide;
->>>>>>> f2437d0 (fix: move landing page to home page)

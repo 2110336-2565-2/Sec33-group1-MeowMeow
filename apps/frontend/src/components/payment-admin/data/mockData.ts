d60cd38072817befdc7b254daf5998472257cda1
TikhampornSky <tikhamporntan@gmail.com>
2023-04-03 10:25:29 +0700

    fix: rebase err

--- apps/frontend/src/components/payment-admin/data/mockData.ts
+++ apps/frontend/src/components/payment-admin/data/mockData.ts
@@ -1,14 +1,14 @@
-import { IData } from "./recordType";
-import { statusType } from "./statusHandle";
+import { IPaymentData } from "./recordType";
+import { statusPaymentType } from "./statusHandle";
 
-function createData(
+function createPaymentData(
   id: string,
   userID: string,
   guideID: string,
   timeStamp: string,
   price: number,
   paymentStatus: string
-): IData {
+): IPaymentData {
   return {
     id,
     userID,
@@ -20,44 +20,44 @@ function createData(
 }
 
 export const rows = [
-  createData(
+  createPaymentData(
     "1",
     "user1-id",
     "guide1-id",
     "2020-01-01T10:30",
     100,
-    statusType.TRAVELLER_PAID
+    statusPaymentType.TRAVELLER_PAID
   ),
-  createData(
+  createPaymentData(
     "2",
     "user2-id",
     "guide2-id",
     "2020-02-02T10:30",
     88.88,
-    statusType.WAITING_REFUND
+    statusPaymentType.WAITING_REFUND
   ),
-  createData(
+  createPaymentData(
     "3",
     "user3-id",
     "guide3-id",
     "2020-03-03T10:30",
     44.55,
-    statusType.REFUNDED
+    statusPaymentType.REFUNDED
   ),
-  createData(
+  createPaymentData(
     "4",
     "user4-id",
     "guide4-id",
     "2020-04-04T10:30",
     99.21,
-    statusType.HOLDING
+    statusPaymentType.HOLDING
   ),
-  createData(
+  createPaymentData(
     "5",
     "user5-id",
     "guide5-id",
     "2020-05-05T10:30",
     55,
-    statusType.GUIDE_GET_PAID
+    statusPaymentType.GUIDE_GET_PAID
   ),
 ];

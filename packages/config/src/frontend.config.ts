7d9c4c0134e0074df67f3607add0fb6fd6f91366
TendonT52 <88754538+TendonT52@users.noreply.github.com>
2023-04-03 13:57:15 +0700

    fix: rename omise env (#129)

--- packages/config/src/frontend.config.ts
+++ packages/config/src/frontend.config.ts
@@ -4,10 +4,18 @@ export type FrontendConfig = {
   BACKEND_BASE_URL: string;
   FRONTEND_BASE_URL: string;
   NEXT_PUBLIC_ABC: string;
+  FRONTEND_OMISE_PUBLIC_KEY: string;
 };
 
-export const loadFrontendConfig = (): FrontendConfig => ({
-  BACKEND_BASE_URL: process.env.BACKEND_BASE_URL || "http://localhost:8080/",
-  FRONTEND_BASE_URL: process.env.FRONTEND_BASE_URL || "http://localhost:3000/",
-  NEXT_PUBLIC_ABC: process.env.NEXT_PUBLIC_ABC || "no env",
-});
+export const loadFrontendConfig = (): FrontendConfig => {
+  if (!process.env.FRONTEND_OMISE_PUBLIC_KEY) {
+    throw new Error("FRONTEND_OMISE_PUBLIC_KEY is not set");
+  }
+  return {
+    FRONTEND_OMISE_PUBLIC_KEY: process.env.FRONTEND_OMISE_PUBLIC_KEY,
+    BACKEND_BASE_URL: process.env.BACKEND_BASE_URL || "http://localhost:8080/",
+    FRONTEND_BASE_URL:
+      process.env.FRONTEND_BASE_URL || "http://localhost:3000/",
+    NEXT_PUBLIC_ABC: process.env.NEXT_PUBLIC_ABC || "no env",
+  };
+};

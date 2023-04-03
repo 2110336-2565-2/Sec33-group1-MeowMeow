9c5ebc549da60afe3ab5c59115dfbe408bca6d30
Pacharapon Arpanantikul <pacharaponarp@gmail.com>
2023-04-04 02:23:52 +0700

    fix: more bug fixes

--- apps/frontend/src/hooks/useDashBoard.ts
+++ apps/frontend/src/hooks/useDashBoard.ts
@@ -1,13 +1,21 @@
 import dashboardUrlMapper from "@/utils/dashboardUrlMapper";
-import { useState } from "react";
+import { useContext, useMemo, useState } from "react";
 import { AVAILABLE_DASHBOARD_STATE } from "./types/dashBoardState";
 import { useRouter } from "next/router";
+import { AuthContext } from "@/context/AuthContext";
 
 const useDashBoard = () => {
   const router = useRouter();
-  const [selectTab, setSelectTab] = useState<AVAILABLE_DASHBOARD_STATE>(
-    dashboardUrlMapper(router.asPath)
-  );
+  const { user } = useContext(AuthContext);
+  const isGuide = useMemo(() => {
+    if (!user || !user.roles) {
+      return false;
+    }
+    return !!user.roles.includes("GUIDE");
+  }, [user]);
+  const [selectTab, setSelectTab] = useState<
+    AVAILABLE_DASHBOARD_STATE | undefined
+  >(dashboardUrlMapper(router.asPath, isGuide));
 
   const onChange = (
     _: React.SyntheticEvent,

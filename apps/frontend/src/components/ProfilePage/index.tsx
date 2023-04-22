d45dc22a1588d6e18cdff69f6f451c0055c75aa9
Polapob <71330294+Polapob@users.noreply.github.com>
2023-04-22 11:38:24 +0700

    fix: remove unused import

--- apps/frontend/src/components/ProfilePage/index.tsx
+++ apps/frontend/src/components/ProfilePage/index.tsx
@@ -1,12 +1,8 @@
 import Typography from "@mui/material/Typography";
-import { Fragment, useCallback, useContext, useState } from "react";
-import Navbar from "../common/Navbar";
-import { IProfileData } from "./types/profilePage";
+import { Fragment, useCallback, useState } from "react";
 import Box from "@mui/material/Box";
 import ShowProfile from "./ShowProfile";
 import EditProfile from "./EditProfile";
-import { AuthContext } from "@/context/AuthContext";
-import { IUser } from "@/context/type/authContext";
 
 const imageUrl =
   "https://images.unsplash.com/photo-1506439773649-6e0eb8cfb237?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";

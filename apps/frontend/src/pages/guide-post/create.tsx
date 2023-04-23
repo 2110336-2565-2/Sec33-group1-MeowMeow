7de6c7a9589208611fc02eaee5891623e71f6434
Tikhamporn Tepsut <88883551+TikhampornSky@users.noreply.github.com>
2023-04-23 17:05:05 +0700

    fix: create/edit post permission (#201)

--- apps/frontend/src/pages/guide-post/create.tsx
+++ apps/frontend/src/pages/guide-post/create.tsx
@@ -1,10 +1,11 @@
 import DashBoard from "@/components/Dashboard/DashBoard";
 import CreateingPost from "@/components/guide-post/creatingPost";
+import { Roles } from "@/context/type/authContext";
 import React from "react";
 
 function GuidePostCreate() {
   return (
-    <DashBoard>
+    <DashBoard roleAllowed={["GUIDE"]}>
       <CreateingPost />
     </DashBoard>
   );
--- apps/frontend/src/pages/guide-post/edit/[id].tsx
+++ apps/frontend/src/pages/guide-post/edit/[id].tsx
@@ -1,10 +1,11 @@
+import DashBoard from "@/components/Dashboard/DashBoard";
 import EditingPost from "@/components/guide-post/editingPost";
-import React from "react";
+import React, { useContext } from "react";
 
 export default function GuidePostEdit() {
   return (
-    <>
+    <DashBoard roleAllowed={["GUIDE"]}>
       <EditingPost />
-    </>
+    </DashBoard>
   );
 }

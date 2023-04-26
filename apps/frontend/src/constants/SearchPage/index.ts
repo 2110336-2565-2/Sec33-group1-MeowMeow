09bc276a7729843b36f965838f604e29bae66cca
Punnapob Jindakul <punnapob.jdk@gmail.com>
2023-04-26 20:38:21 +0700

    fix: fix backend post search & frontend manageTripGuide & minRating default

--- apps/frontend/src/constants/SearchPage/index.ts
+++ apps/frontend/src/constants/SearchPage/index.ts
@@ -3,7 +3,7 @@ import { IFilterOptions } from "@/components/SearchPage/types";
 export const POST_PER_PAGE = 5;
 
 export const maxFee: number = 10000;
-export const minRating: number = 5;
+export const minRating: number = 0;
 export const stepFee: number = 100;
 export const stepRating: number = 1;
 
